7e2a995a84d4e72233ad1f5143e273766aa84a50
Ondrej Harnúšek <ondrej.harnusek@gmail.com>
2024-05-13 18:20:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const port = 9000;
 const axios = require("axios");
 
 app.get("/", (req, res) => {
+  const host = "http://0.0.0.0:3003";
   let config = {
     method: "get",
     maxBodyLength: Infinity,
-    url: "http://umbrel.local:3003/api/v1/invoices?storeId=2oQ2SP5NyFzdPtybZuiLTXF2Jq59S3WhYL4x1iSgxzhi&currency=EUR&defaultPaymentMethod=BTC_LNURLPAY",
+    url: `${host}/api/v1/invoices?storeId=2oQ2SP5NyFzdPtybZuiLTXF2Jq59S3WhYL4x1iSgxzhi&currency=EUR&defaultPaymentMethod=BTC_LNURLPAY`,
     headers: {},
   };
 
@@ -16,10 +17,7 @@ app.get("/", (req, res) => {
     .request(config)
     .then((response) => {
       const localUrl = response.request.res.responseUrl;
-      const url = localUrl.replace(
-        "http://umbrel.local:3003",
-        "https://btc.hrn.sk"
-      );
+      const url = localUrl.replace(host, "https://btc.hrn.sk");
       res.redirect(url);
     })
     .catch((error) => {
